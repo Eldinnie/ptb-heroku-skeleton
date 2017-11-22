1a6a8de5990f2f9d1f18b5d2d7cb7980e75f13e4
Eldin <pieter.schutz@gmail.com>
2017-11-22 18:37:55 +0100

    init

---
+++ herokubot.py
@@ -0,0 +1,29 @@
+import os
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+def start(bot, update):
+    update.effective_message.reply_text("Hi!")
+
+
+def echo(bot, update):
+    update.effective_message.reply_text(update.effective_message.text)
+
+
+if __name__ == "__main__":
+    TOKEN = "Your token from @Botfather"
+    NAME = "The name of your app on Heroku"
+
+    port = os.environ.get('PORT')
+
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(port),
+                          url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    updater.idle()
