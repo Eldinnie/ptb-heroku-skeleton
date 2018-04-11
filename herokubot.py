28ba8c29eae8bc09d67d249432ef42cc5a7f4df2
Eldinnie <Eldinnie@users.noreply.github.com>
2018-04-11 22:34:39 +0200

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     # Add handlers
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(MessageHandler(Filters.text, echo))
-    dp.add_handler(error)
+    dp.add_error_handler(error)
 
     # Start the webhook
     updater.start_webhook(listen="0.0.0.0",
